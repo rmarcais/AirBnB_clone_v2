5d2a1b175b43fceeaedf998bc6778e6496a52e30
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-11 16:29:31 +0200

    Add file for task 7

--- models/user.py
+++ models/user.py
@@ -4,12 +4,14 @@ This module defines the class User.
 """
 
 
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines the user's informations"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)

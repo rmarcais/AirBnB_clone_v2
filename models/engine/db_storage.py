84a60e0e7d3ddd131a975b66ccf22d8edde2c43e
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-11 09:41:11 +0200

    Add files for task 6

---
+++ models/engine/db_storage.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""New engine DBStorage"""
+
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+all_classes = {'State': State, 'City': City,
+               'User': User, 'Place': Place,
+               'Review': Review, 'Amenity': Amenity}
+
+
+class DBStorage:
+    """Represents a database storage engine.
+    Attributes:
+        __engine (sqlalchemy.Engine): The working SQLAlchemy engine.
+        __session (sqlalchemy.Session): The working SQLAlchemy session.
+    """
+
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Initialize a new DBStorage instance."""
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
+        format(getenv("HBNB_MYSQL_USER"),
+        getenv("HBNB_MYSQL_PWD"),
+        getenv("HBNB_MYSQL_HOST"),
+        getenv("HBNB_MYSQL_DB")),
+        pool_pre_ping=True)
+        if getenv("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """Query and return all objects by class/generally
+        Return: dictionary (<class-name>.<object-id>: <obj>)
+        """
+        obj_dict = {}
+
+        if cls:
+            for row in self.__session.query(cls).all():
+                # populate dict with objects from storage
+                obj_dict.update({'{}.{}'.
+                                format(type(cls).__name__, row.id,): row})
+        else:
+            for key, val in all_classes.items():
+                for row in self.__session.query(val):
+                    obj_dict.update({'{}.{}'.
+                                    format(type(row).__name__, row.id,): row})
+        return obj_dict
+
+    def new(self, obj):
+        """Add obj to the current database session."""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commit all changes to the current database session."""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Delete obj from the current database session."""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Create all tables in the database and initialize a new session."""
+        Base.metadata.create_all(self.__engine)
+        session = sessionmaker(bind=self.__engine,
+        expire_on_commit=False)
+        self.__session = scoped_session(session)
+
+    def close(self):
+        """Close the working SQLAlchemy session."""
+        self.__session.close()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from os import getenv
+from tkinter import CASCADE
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        cities = relationship("City", backref="state", cascade='all, delete')
+    
+    else:
+        @property
+        def cities(self):
+            """getter attribute that returns the
+            list of City instances with state_id equals to
+            the current State.id"""
+            l = []
+            dico = models.storage.all(City)
+            for key, value in dico:
+                if value.state_id == self.id:
+                    l.append(value)
+            return l

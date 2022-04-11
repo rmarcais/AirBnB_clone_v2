7c710a1f7fbc962adbc3f3aa54797aafbe751c61
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-11 11:50:48 +0200

    Edit states

--- models/state.py
+++ models/state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from os import getenv
-from tkinter import CASCADE
 from models.base_model import BaseModel, Base
 from models.city import City
 from sqlalchemy import Column, Integer, String
@@ -15,7 +14,7 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     if getenv('HBNB_TYPE_STORAGE') == "db":
-        cities = relationship("City", backref="state", cascade='all, delete')
+        cities = relationship("City", backref="state")
     else:
         @property
         def cities(self):

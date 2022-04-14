43374c2f3279c19348e80504de6d573b8939a0ec
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-14 20:26:04 +0200

    Add tests for console

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,14 +1,42 @@
 #!/usr/bin/python3
 import pycodestyle
 import unittest
-from models import BaseModel
+import console
+from console import HBNBCommand
 
 
-class test_console(BaseModel):
+class test_console(unittest.TestCase):
     """Test class for db storage"""
+
+    def test_doc(self):
+        """Tests the documentation"""
+
+        self.assertGreater(len(console.__doc__), 0)
+
+        self.assertGreater(len(HBNBCommand.__doc__), 0)
+
+        self.assertGreater(len(HBNBCommand.do_all.__doc__), 0)
+
+        self.assertGreater(len(HBNBCommand.do_create.__doc__), 0)
+
+        self.assertGreater(len(HBNBCommand.do_destroy.__doc__), 0)
+
+        self.assertGreater(len(HBNBCommand.do_quit.__doc__), 0)
+
+        self.assertGreater(len(HBNBCommand.do_EOF.__doc__), 0)
+
+        self.assertGreater(len(HBNBCommand.do_count.__doc__), 0)
+
+        self.assertGreater(len(HBNBCommand.do_destroy.__doc__), 0)
+
+        self.assertGreater(len(HBNBCommand.do_update.__doc__), 0)
+
+        self.assertGreater(len(HBNBCommand.emptyline.__doc__), 0)
+
+        self.assertGreater(len(HBNBCommand.default.__doc__), 0)
 
     def test_pep8_console(self):
         """pep8 test"""
         style = pycodestyle.StyleGuide(quiet=True)
-        p = style.check_files(['models/engine/test_console.py'])
+        p = style.check_files(['console.py'])
         self.assertEqual(p.total_errors, 0, "fix pep8")

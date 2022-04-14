b695550a6ab0d651f274f44692c04357b7c06e11
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-14 20:13:00 +0200

    Fix bugs

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,24 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.8
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
-import pycodestyle
-import os
+from os import getenv
+
+storageType = getenv("HBNB_TYPE_STORAGE")
 
 
 class test_Amenity(test_basemodel):
-    """ tests for Amenitiy"""
-
-    @classmethod
-    def setUpClass(cls):
-        """set up for test"""
-        cls.amenity = Amenity()
-        cls.amenity.name = "Breakfast"
-
-    @classmethod
-    def teardown(cls):
-        """at the end of the test this will tear it down"""
-        del cls.amenity
+    """ """
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -26,31 +16,7 @@ class test_Amenity(test_basemodel):
         self.name = "Amenity"
         self.value = Amenity
 
-    def teardown(self):
-        """ """
-        try:
-            os.remove("file.json")
-        except Exception:
-            pass
-
     def test_name2(self):
         """ """
-        self.assertEqual(type(self.name), str)
-
-    def test_pep8_DBStorage(self):
-        """pep8 test"""
-        style = pycodestyle.StyleGuide(quiet=True)
-        p = style.check_files(['models/engine/db_storage.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
-
-    def test_save(self):
-        """ """
-        self.amenity.save()
-        self.assertNotEqual(self.amenity.created_at, self.amenity.updated_at)
-
-    def test_attr(self):
-        """ """
-        self.assertTrue('id' in self.amenity.__dict__)
-        self.assertTrue('created_at' in self.amenity.__dict__)
-        self.assertTrue('updated_at' in self.amenity.__dict__)
-        self.assertTrue('name' in self.amenity.__dict__)
+        new = self.value()
+        self.assertEqual(new.name, None)

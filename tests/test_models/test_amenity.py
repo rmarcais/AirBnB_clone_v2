97dee4e7d1c1ec66f5ffc8bebd2cee5a7c271904
Anthony-6 <anthony.lacoste5@gmail.com>
2022-04-14 18:10:00 +0200

    update test_amenity file

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -3,6 +3,7 @@
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 import pycodestyle
+import os
 
 
 class test_Amenity(test_basemodel):
@@ -13,6 +14,13 @@ class test_Amenity(test_basemodel):
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
+
+    def teardown(self):
+        """ """
+        try:
+            os.remove()
+        except Exception:
+            pass
 
     def test_name2(self):
         """ """

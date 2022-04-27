2af5e277e5fec7a90923998ddae1913ec3b8be93
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-27 17:46:18 +0200

    Improve code

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,17 +13,16 @@ from os import path
 
 def do_pack():
     """Function that does a pack with the files of the folder web_static."""
-    try:
-        if path.exists('versions/') is False:
-            local("mkdir versions/")
-        filename = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-            stf("%Y"),
-            stf("%m"),
-            stf("%d"),
-            stf("%H"),
-            stf("%M"),
-            stf("%S"))
-        local("tar -cvzf {} web_static".format(filename))
-        return filename
-    except Exception:
+    if path.exists('versions/') is False:
+        local("mkdir versions/")
+    filename = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        stf("%Y"),
+        stf("%m"),
+        stf("%d"),
+        stf("%H"),
+        stf("%M"),
+        stf("%S"))
+    a = local("tar -cvzf {} web_static".format(filename))
+    if a.failed is True:
         return None
+    return filename
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,25 +17,39 @@ env.hosts = ['34.74.115.199', '34.74.58.247']
 
 def do_deploy(archive_path):
     """ Function that deploys archive path. """
-    try:
-        if path.exists(archive_path) is False:
-            return False
-
-        no_ver = archive_path.split("/")
-        no_ext = no_ver[1].split(".")
-        p = "/data/web_static/releases"
-
-        put("versions/{}".format(no_ver[1]), "/tmp/{}".format(no_ver[1]))
-        run("mkdir -p /data/web_static/releases/{}".format(no_ext[0]))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
-            no_ver[1], no_ext[0]))
-        run("rm /tmp/{}".format(no_ver[1]))
-        run("mv {}/{}/web_static/* {}/{}/".format(p, no_ext[0], p, no_ext[0]))
-
-        run("rm -rf /data/web_static/releases/{}/web_static".format(no_ext[0]))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(no_ext[0]))
-        return True
-    except Exception:
+    if path.exists(archive_path) is False:
         return False
+
+    no_ver = archive_path.split("/")
+    no_ext = no_ver[1].split(".")
+    p = "/data/web_static/releases"
+
+    a = put("versions/{}".format(no_ver[1]), "/tmp/{}".format(no_ver[1]))
+    if a.failed is True:
+        return False
+    a = run("mkdir -p /data/web_static/releases/{}".format(no_ext[0]))
+    if a.failed is True:
+        return False
+    a = run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
+        no_ver[1], no_ext[0]))
+    if a.failed is True:
+        return False
+    a = run("rm /tmp/{}".format(no_ver[1]))
+    if a.failed is True:
+        return False
+    a = run("mv {}/{}/web_static/* {}/{}/"
+            .format(p, no_ext[0], p, no_ext[0]))
+    if a.failed is True:
+        return False
+    a = run("rm -rf /data/web_static/releases/{}/web_static"
+            .format(no_ext[0]))
+    if a.failed is True:
+        return False
+    a = run("rm -rf /data/web_static/current")
+    if a.failed is True:
+        return False
+    a = run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(no_ext[0]))
+    if a.failed is True:
+        return False
+    return True

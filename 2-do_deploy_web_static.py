9dc3a741a17440d4abc9874ca86202cc01622f65
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-27 14:37:09 +0200

    Add 2-do_deploy_web_static.py

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Fabric script (based on the file 1-pack_web_static.py) that
+distributes an archive to your web servers, using the function do_deploy.
+"""
+
+
+from fabric.api import local
+from fabric.api import run
+from fabric.api import put
+from fabric.api import env
+import os.path
+from os import path
+
+env.hosts = ['34.74.115.199', '34.74.58.247']
+
+
+def do_deploy(archive_path):
+    """ Function that deploys archive path. """
+    try:
+        if path.exists(archive_path) is False:
+            return False
+
+        no_ver = archive_path.split("/")
+        no_ext = no_ver[1].split(".")
+        p = "/data/web_static/releases"
+
+        put("versions/{}".format(no_ver[1]), "/tmp/{}".format(no_ver[1]))
+        run("mkdir -p /data/web_static/releases/{}".format(no_ext[0]))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
+            no_ver[1], no_ext[0]))
+        run("rm /tmp/{}".format(no_ver[1]))
+        run("mv {}/{}/web_static/* {}/{}/".format(p, no_ext[0], p, no_ext[0]))
+
+        run("rm -rf /data/web_static/releases/{}/web_static".format(no_ext[0]))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(no_ext[0]))
+        return True
+    except Exception:
+        return False

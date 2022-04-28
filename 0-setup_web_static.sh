524661b8f74a4136ba56dd5c72445cc5f16379fc
Rémi Marçais <remi.marcais87@gmail.com>
2022-04-28 09:47:18 +0200

    Improve code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,11 +2,10 @@
 # Bash script that sets up your web servers for the deployment of web_static
 sudo apt-get -y update
 sudo apt-get -y install nginx
-mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Hello Holberton !" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/
 sed -i '/server_name _;/a location /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 sudo service nginx restart

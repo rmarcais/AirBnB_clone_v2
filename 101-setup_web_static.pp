9996d07f8e9bdb0970f0561b2fffa34294bce3b8
Rémi Marçais <remi.marcais87@gmail.com>
2022-05-01 18:40:35 +0200

    Add 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -5,43 +5,43 @@ command => 'apt-get -y update',
 path    => '/usr/bin/',
 }
 
-package { 'nginx':
+-> package { 'nginx':
 ensure   => 'installed',
 provider => 'apt',
 }
 
-exec { '3':
+-> exec { '3':
 command => 'mkdir -p /data/web_static/releases/test/',
 path    => '/usr/bin/',
 }
 
-exec { '4':
+-> exec { '4':
 command => 'mkdir -p /data/web_static/shared/',
 path    => '/usr/bin/',
 }
 
-file { '/data/web_static/releases/test/index.html':
+-> file { '/data/web_static/releases/test/index.html':
 ensure  => 'present',
 path    => '/data/web_static/releases/test/index.html',
 content => 'Hello Holberton !\n',
 }
 
-exec { '6':
+-> exec { '6':
 command => 'ln -sf /data/web_static/releases/test/ /data/web_static/current',
 path    => '/usr/bin/',
 }
 
-exec { '7':
+-> exec { '7':
 command => 'sudo chown -R ubuntu:ubuntu /data/',
 path    => '/usr/bin/',
 }
 
-exec { '8':
+->exec { '8':
 command => "sed -i '47i\\tlocation /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-available/default",
 path    => '/usr/bin/',
 }
 
-service { 'nginx':
+-> service { 'nginx':
 ensure  => 'running',
 require => Package['nginx'],
 }

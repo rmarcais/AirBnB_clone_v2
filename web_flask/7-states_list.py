f6aa7d4ffdf5fcb9dd0cf07df41e92e31b7f9e26
Rémi Marçais <remi.marcais87@gmail.com>
2022-05-12 15:57:05 +0200

    Add files for task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application. """
+
+from flask import Flask, escape, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    list_s = []
+    dico = storage.all(State)
+    for k, v in dico.items():
+        list_s.append(dico[k])
+    list_o = sorted(list_s, key=lambda d: d.name)
+    return render_template('7-states_list.html', list_s=list_o)
+
+
+@app.teardown_appcontext
+def tear_down(exception):
+    """ Method that reloves the current SQLAlchemy Session. """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')

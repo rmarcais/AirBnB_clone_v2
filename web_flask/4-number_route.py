c71439d89d544b0754f1bad8a19089f9df5d6a46
Rémi Marçais <remi.marcais87@gmail.com>
2022-05-12 11:21:09 +0200

    Add 4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application. """
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Function tat returns a message. """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Function tat returns a message. """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """ Function tat returns a message. """
+    text = text.replace("_", " ")
+    return "C %s" % escape(text)
+
+
+@app.route('/python/', strict_slashes=False, defaults={'text': 'is_cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is_cool"):
+    """ Function tat returns a message. """
+    text = text.replace("_", " ")
+    return "Python %s" % escape(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """ Function tat returns a message. """
+    return "%d is a number" % n
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')

535d9d8074c54ee36237a70ba7934cdc08f500a3
Rémi Marçais <remi.marcais87@gmail.com>
2022-05-12 10:36:01 +0200

    Add 1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application. """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Function tat returns a message. """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Function tat returns a message. """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')

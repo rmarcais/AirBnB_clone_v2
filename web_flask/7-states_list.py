bb15c69f9fbc71d6445e5e5591819a801770e2dd
Rémi Marçais <remi.marcais87@gmail.com>
2022-05-14 00:42:40 +0200

    Fix bugs

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application.
+""" Script that starts a Flask web application. """
 
 from flask import Flask, escape, render_template
 from models import storage
@@ -10,42 +10,16 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-     Returns a template that lists all State present in DBStorage.
+    """ Returns a template that lists all State present in DBStorage. """
     return render_template('7-states_list.html',
                            list_s=storage.all(State).values())
 
 
 @app.teardown_appcontext
 def tear_down(self):
-     Method that reloves the current SQLAlchemy Session.
+    """ Method that reloves the current SQLAlchemy Session. """
     storage.close()
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
-"""
-"""Starts a Flask web application"""
-
-from models import storage
-from models.state import State
-from flask import Flask
-from flask import render_template
-app = Flask(__name__)
-
-
-@app.route('/states_list', strict_slashes=False)
-def states():
-    """Returns a rendered html template
-    at the /states_list route,
-    listing all states"""
-    return render_template('7-states_list.html',
-                           states=storage.all('State').values())
-
-
-@app.teardown_appcontext
-def teardown(self):
-    """Removes the current SQLAlchemy Session"""
-    storage.close()
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')

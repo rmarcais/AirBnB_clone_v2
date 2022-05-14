4ab1951ae3d0eeed9148939dd802f83f04a7a7a9
Rémi Marçais <remi.marcais87@gmail.com>
2022-05-14 15:45:27 +0200

    Fix bugs

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application"""
-
-from flask import Flask
+""" Script that starts a Flask web application. """
+from flask import Flask, escape, render_template
 from models import storage
 from models.state import State
-from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def state_list_route():
-    """List all the State object alphabetically"""
-    list_s = []
-    dico = storage.all(State)
-    for k, v in dico.items():
-        list_s.append(dico[k])
-    return render_template('7-states_list.html', states=list_s)
+def states_list_route():
+    """ Returns a template that lists all State present in DBStorage. """
+    return render_template('7-states_list.html',
+                           list_s=storage.all(State).values())
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """Removes the current SQLAlchemy Session"""
+def tear_down(self):
+    """ Method that reloves the current SQLAlchemy Session. """
     storage.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0')

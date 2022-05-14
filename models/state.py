ccb318b59b626a71252e07f440d928aa0e4ac25b
Rémi Marçais <remi.marcais87@gmail.com>
2022-05-14 13:26:29 +0200

    Fix bugs

--- models/state.py
+++ models/state.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""state.py
+This module defines the class State.
+"""
 import models
 from models.base_model import BaseModel, Base
-from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
+from models.city import City
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
+    """This class defines the name of the state"""
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    if getenv('HBNB_TYPE_STORAGE') == "db":
-        cities = relationship("City", backref="state", cascade="all, delete")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state")
     else:
         @property
         def cities(self):
-            """"""
-            my_list = []
-            all_city = models.storage.all(City)
-            for key, value in all_city.items():
-                if value.state_id == self.id:
-                    my_list.append(value)
-            return my_list
+            """cities attribute"""
+            liste = []
+            objs = models.storage.all(City)
+            for k, v in objs.items():
+                if v.state_id == self.id:
+                    liste.append(v)
+            return liste
